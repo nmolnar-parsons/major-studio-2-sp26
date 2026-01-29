d253647eacf24c61b67049a6028dc1b382ebedfb
Nichos Molnar <molnn656@newschool.edu>
2026-01-29 19:51:04 +0000

    in-class work

--- 00-html-review/warmup/js/main.js
+++ 00-html-review/warmup/js/main.js
@@ -1,5 +1,34 @@
 function setup() {
   // TODO: Setup buttons and counter
+  const button_1 = document.getElementById("butt-1");
+  const button_2 = document.getElementById("butt-2");
+  const counter1 = document.getElementById("counter1");
+  const counter2 = document.getElementById("counter2");
+
+  const overallCounter = document.getElementById("counter-total");
+
+  button_1.addEventListener("click", (evt) =>{
+    counter1.innerHTML = parseInt(counter1.innerHTML) + 1;
+    overallCounter.innerHTML = parseInt(overallCounter.innerHTML) + 1;
+
+
+    console.log("added 1");
+  });
+  button_2.addEventListener("click", (evt) =>{
+    counter2.innerHTML = parseInt(counter2.innerHTML) + 2;
+    overallCounter.innerHTML = parseInt(overallCounter.innerHTML) + 2;
+
+
+
+    console.log("added 2");
+  });
+
+  // expanding this for much larger numbers of buttons and counters gets very difficulty quickly
+  // hard to scale. What button goes with what, what counter goes with what...
+  // d3 is a way to do this with SVG elements, but not html, per se
+  // how do we treat HTML/JS pairings at scale?
+
 }
 
 document.addEventListener("DOMContentLoaded", setup);
+// makes sure the DOM is loaded before changes are made to it - basically a safeguard function
--- 01-vue-intro/warmup/warmup.js
+++ 01-vue-intro/warmup/warmup.js
@@ -4,11 +4,21 @@ const App = {
   data() {
     return {
       // TODO: declare reactive variables
+      counter: [0 ,0 ,0 ], // this stuff is visible to HTML // html is updated automatically, instead of having to write code for it 
     };
   },
 
   methods: {
     // TODO: helper functions
+    butt1clicked(evt) {
+      this.counter[0] += 1; //look for counter variable within "this" app
+      this.counter[2] += 1;
+    },
+    butt2clicked(evt) {
+      this.counter[1] += 2;
+      this.counter[2] +=2;
+    }
+ 
   },
 
   computed: {
@@ -17,4 +27,5 @@ const App = {
   },
 };
 
-Vue.createApp(App).mount("#app");
+Vue.createApp(App).mount("#app"); // pick element from HTML -> in our case, div with buttons and counters
+// vue will find code, 
